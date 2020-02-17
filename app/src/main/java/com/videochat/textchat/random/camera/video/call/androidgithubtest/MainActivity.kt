aea386f8d693d08714e8524a9f4bedadd245a410
ShivaReddi <shivaprasad6606@gmail.com>
2020-02-17 22:04:49 +0530

    Initial commit

---
+++ app/src/main/java/com/videochat/textchat/random/camera/video/call/androidgithubtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.videochat.textchat.random.camera.video.call.androidgithubtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
